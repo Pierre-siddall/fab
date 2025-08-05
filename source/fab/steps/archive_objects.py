65e2d7d02262a50672deb2e727790e8ee40a23f9
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2025-08-05 13:11:07 +0100

    Update type hinting for archive objects parameters

--- source/fab/steps/archive_objects.py
+++ source/fab/steps/archive_objects.py
@@ -34,7 +34,7 @@ DEFAULT_SOURCE_GETTER = CollectionGetter(ArtefactSet.OBJECT_FILES)
 @step
 def archive_objects(config: BuildConfig,
                     source: Optional[ArtefactsGetter] = None,
-                    output_fpath=None,
+                    output_fpath: Optional[str]=None,
                     output_collection=ArtefactSet.OBJECT_ARCHIVES):
     """
     Create an object archive for every build target, from their object files.
