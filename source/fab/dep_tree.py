87e999639a182f8054b81ffff5ca59c6069e4983
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-04-07 11:37:15 +0100

    Remove pytest error

--- source/fab/dep_tree.py
+++ source/fab/dep_tree.py
@@ -1,4 +1,4 @@
-t##############################################################################
+##############################################################################
 # (c) Crown copyright Met Office. All rights reserved.
 # For further details please refer to the file COPYRIGHT
 # which you should have received as part of this distribution
